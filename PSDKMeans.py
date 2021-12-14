ca4fab0cb89c5aa9a9c5dbeb4e730eb161aa6dc2
rookie <623034792@qq.com>
2021-12-14 11:37:38 +0800

    implement  the PSDKMeans

--- PSDKMeans.py
+++ PSDKMeans.py
@@ -4,20 +4,23 @@
  #4.每个节点更新中心点。
  #5.每个节点更新拉格朗日乘数。
  # 重复2-5 直到达到收敛（最大迭代次数）
- # next peer 意为下一个邻居
+ # next peer 意为下一个邻居,不是很懂什么意思（一跳的邻居）
+ # 对所有本地数据求出Dunn指标，然后求他们的平均值,不公平(对所有站点的数据进行汇总，然后用kmeans聚合得出结果)
+ #问题：聚类之后，不同的中心点变成一样的了（原论文更新中心点的地方，除数是站点数量，现论文是处理数据总量）
 
 from mpi4py import MPI
 import numpy as np
-from KMeans import initCentroid, DBIndex, Assign, DunnIndex
-maxIte=10  #最大迭代次数
-k=3#聚类个数
+from KMeans import initCentroid, DBIndex, Assign, DunnIndex, Kmeans
+from DKmeans import Add
+maxIte=25  #最大迭代次数
+k=8#聚类个数
 
 def UpdateCentroidsAndLag(data, label, allCentroids, allLag, oldCentroids, oldLag, u, rank, size):
     length = len(data)
     dimension = len(oldCentroids[0])
     centroid = np.zeros([k, dimension], dtype = float)
     L = np.zeros([k, dimension], dtype = float)
-    nextPeer = (rank - 1 + size) % size
+    nextPeer = (rank + 1 + size) % size
     for i in range(k):
         #更新中心点
         sumPoint = np.zeros(dimension, dtype = float)
@@ -28,8 +31,8 @@ def UpdateCentroidsAndLag(data, label, allCentroids, allLag, oldCentroids, oldLa
                 n+=1
         sumCentroids = np.zeros(dimension, dtype = float)
         for j in range(size):
-            sumCentroids += 2 * u * oldCentroids[i] - oldLag[i] + allLag[j][i]
-        centroid[i] = (sumPoint + sumCentroids) / (2 * u * len(data) + n)
+            sumCentroids += 2 * u * oldCentroids[i] - oldLag[i] + allLag[(j - 1 + size) % size][i]
+        centroid[i] = (sumPoint + sumCentroids) / (2 * u * size + n)
         #更新拉格朗日乘数
         L = L + u * (centroid[i] - allCentroids[nextPeer][i])
     return centroid, L
@@ -38,25 +41,25 @@ def PSDKM():
     comm = MPI.COMM_WORLD
     rank = comm.Get_rank()
     size = comm.Get_size()
-    data = np.loadtxt('dataset/seeds/seeds%d.txt'%rank)
-    allData = np.loadtxt('dataset/seeds/seeds.txt')
+    data = np.loadtxt('dataset/2d8c/2d8c%d.txt'%rank)
+    allData = np.loadtxt('dataset/2d8c/2d8c.txt')
     dimension = len(data[0])
     length = len(data)
     centroids = initCentroid(k,data)
     label = Assign(centroids,data)
-    u = np.random.random_sample()
-    #u = 0.6
+    #u = np.random.random_sample()
+    u = 0.6
     print('this is process%d 初始化的L为'%rank)
     #初始化拉格朗日乘数
     L = np.zeros([k,dimension], dtype = float)
     for i in range(k):
         for j in range(dimension):
             L[i][j]=centroids[i][j]
+    allCentroids = {}
+    allLag = {}
     for i in range(maxIte):
         label = Assign(centroids, data)
-        allCentroids = {}
         allCentroids[rank] = centroids
-        allLag = {}
         allLag[rank] = L
         #向所有邻居发送中心点
         for j in range(size):
@@ -69,22 +72,15 @@ def PSDKM():
                 allCentroids[j] = comm.recv(source = j, tag = 1)
                 allLag[j] = comm.recv(source = j, tag = 2)
         
-        centroids,L = UpdateCentroidsAndLag(data, label, allCentroids, allLag, centroids, L , u, rank, size)
-    #问题：聚类之后，不同的中心点变成一样的了
-        if rank==0:
-            print('this is ite %d'%i)
-            print(centroids)
-        #print(DBIndex(allData, Assign(centroids, allData), centroids))
-        #print(allLag)
-
-def Test1():
-    a=np.zeros([2,2])
-    a[0][0]=1
-    b=np.zeros(2)
-    b=a[0]
-    a[0][0]=2
-    print(b)
+        centroids, L = UpdateCentroidsAndLag(data, label, allCentroids, allLag, centroids, L , u, rank, size)
+    print(DBIndex(data,Assign(centroids,data),centroids))
+    #交给rank为0的进程对所有中心点进行汇总
+    if rank==0:
+        finalcentroid=Add(allCentroids[0],allCentroids[1])
+        for i in range(2,size):
+            finalcentroid=Add(finalcentroid,allCentroids[i])
+        centroids=Kmeans(k,finalcentroid)
+        print(DBIndex(allData,Assign(centroids,allData),centroids))
 
 if __name__=="__main__":
     PSDKM()
-    #Test1()
